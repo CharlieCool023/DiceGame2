b9b4e26ea43f87562d2783ab66011fba792d6129
Charles Onah <105295419+CharlieCool023@users.noreply.github.com>
2022-06-24 19:41:36 +0100

    CharlieCool Dice Game 2

---
+++ index.js
@@ -0,0 +1,25 @@
+// document.querySelector("h1").classList.toggle("invisible");
+// document.querySelector("h1").innerHTML("Player1");
+
+var randomNumber1 = (Math.floor(Math.random() * 6)+1);
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomImageSource = "images/" +randomDiceImage;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = (Math.floor(Math.random() * 6)+1);
+var randomDiceImage = "dice" + randomNumber2 + ".png";
+var randomImageSource = "images/" +randomDiceImage;
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImageSource);
+
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 wins 🚩";
+}else if (randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 wins 🚩";
+}else if (randomNumber1 = randomNumber2){
+    document.querySelector("h1").innerHTML = "Draw";
+}else{
+    document.querySelector("h1");
+}
